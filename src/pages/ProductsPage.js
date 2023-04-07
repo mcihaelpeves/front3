18d8135f5db138df34302847b434640d3c84e5d2
michaelpeves <michaelpeves@icloud.com>
2023-04-07 18:45:59 -0500

    first

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,11 +1,12 @@
 import { Helmet } from 'react-helmet-async';
 import { useState } from 'react';
 // @mui
-import { Container, Stack, Typography } from '@mui/material';
+import { Button, Container, Stack, Typography } from '@mui/material';
 // components
 import { ProductSort, ProductList, ProductCartWidget, ProductFilterSidebar } from '../sections/@dashboard/products';
 // mock
 import PRODUCTS from '../_mock/products';
+
 
 // ----------------------------------------------------------------------
 
@@ -30,6 +31,8 @@ export default function ProductsPage() {
         <Typography variant="h4" sx={{ mb: 5 }}>
           Products
         </Typography>
+
+        <Button>DATOS</Button>
 
         <Stack direction="row" flexWrap="wrap-reverse" alignItems="center" justifyContent="flex-end" sx={{ mb: 5 }}>
           <Stack direction="row" spacing={1} flexShrink={0} sx={{ my: 1 }}>
